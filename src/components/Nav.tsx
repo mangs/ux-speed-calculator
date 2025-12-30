818c0a497bfb21cfff5838dcd5a533eca8dd8b3e
Alexander Chernyshev <33011221+AlexanderChernyshev@users.noreply.github.com>
2025-12-30 18:30:30 -0500

    nav styling, getting used to solid and tailwind

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,12 +5,12 @@ function Nav() {
   const active = (path: string) =>
     path === location.pathname ? 'border-sky-600' : 'border-transparent hover:border-sky-600';
   return (
-    <nav class="bg-sky-800">
-      <ul class="container flex items-center p-3 text-gray-200">
-        <li class={`border-b-2 ${active('/')} mx-1.5 sm:mx-6`}>
+    <nav class="bg-orange-700">
+      <ul class="container flex items-center p-1 text-green-50">
+        <li class={`font-[1000] ${active('/')} mx-1.5 sm:mx-6`}>
           <a href="/">Home</a>
         </li>
-        <li class={`border-b-2 ${active('/about')} mx-1.5 sm:mx-6`}>
+        <li class={`font-[1000] ${active('/about')} mx-1.5 sm:mx-6`}>
           <a href="/about">About</a>
         </li>
       </ul>
